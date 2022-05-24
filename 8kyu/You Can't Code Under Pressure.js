139bd1bd463754a4056f8023e877233e1f9a44b9
KyngCoder <keshawnrkmerchant9@gmail.com>
2022-05-24 07:57:20 -0500

    solution

---
+++ 8kyu/You Can't Code Under Pressure.js
@@ -0,0 +1,12 @@
+//my solution
+
+function doubleInteger(i) {
+  
+    return i*2;
+  }
+
+//best practice
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2;
+  }
